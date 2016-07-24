21940bbfd3918fe13785d16438ad5eebc48e0807
BlackRockSoul <sanek-ekb@mail.ru>
2016-07-24 08:42:08 +0500

    Добавлено несколько новых плавающих блоков. Усложнена система с плавающими блоками, увеличено их количество. Немного упрощен css файл для этих блоков. Исправлен и немного упрощен вызов функции по прокрутке. Добавлена функция для некого параллакс эффекта

--- index.php
+++ index.php
@@ -1,37 +1,55 @@
 <html>
 
 <?php include ("inc/header.php"); ?>
-<?php include ("inc/menu.php"); ?>
+	<?php include ("inc/menu.php"); ?>
 
-<body>
-	<main>
-		<div class="home_main" id="home_main_1">
-			<div class="home_main" id="home_main_2"></div>
-		</div>
-		<div class="main_text">
-			Попробуй сайт на вкус
-			<div class="main_text_sub">
-				Окунись в сайтостроение, падаван
-				<span class="main_button">
+		<body>
+			<main>
+				<div class="home_main" id="home_main_1">
+					<div class="home_main" id="home_main_2"></div>
+				</div>
+				<div class="main_text">
+					Попробуй сайт на вкус
+					<div class="main_text_sub">
+						Окунись в сайтостроение, падаван
+						<span class="main_button">
 					<a rel="nofollow" href="projects.php">Окунуться в это болото</a>
 				</span>
-			</div>
-		</div>
-	</main>
-	<sub_1 class="sub_block1 blck0">
-		<div class="sub_block">
-			Тут что-нибудь наверняка будет<br>
-			Не доделано ещё, чего не ясно!?
-		</div>
-	</sub_1>
-	<sub_2 class="sub_block1 blck1">
-		<div class="sub_block">
-			Ну скоро, скоро...<br>
-			Погоди немного!
-		</div>
-	</sub_2>
-</body>
+					</div>
+				</div>
+			</main>
+			<sub_1 class="sub_block1 blck1">
+				<div class="sub_text1 sub_set">
+					Тут что-нибудь наверняка будет. Например раздел "Обо мне"? :)
+					<br> Не доделано ещё, но скоро всё будет!
+				</div>
+			</sub_1>
+			<sub_2 class="sub_block1 blck2">
+				<div class="sub_text2 sub_set">
+					А тут может быть раздел "Проекты" сделаю
+					<br> Почему бы и нет?!
+				</div>
+			</sub_2>
+			<sub_3 class="sub_block1 blck1">
+				<div class="sub_text3 sub_set">
+					И сюда что-нибудь придумаю,
+					<br> Не переживай! Лучше дизайн оцени :)
+				</div>
+			</sub_3>
+			<sub_4 class="sub_block1 blck2">
+				<div class="sub_text4 sub_set">
+					Обратная связь тут будет
+					<br> Хорошая идея?
+				</div>
+			</sub_4>
+			<sub_5 class="sub_block1 blck1">
+				<div class="sub_text5 sub_set">
+					Нижнее меню
+					<br> Многовато места наверное оставил... Но, думаю сойдет.
+				</div>
+			</sub_5>
+		</body>
 
-<?php include ("inc/footer.php"); ?>
+		<?php include ("inc/footer.php"); ?>
 
 </html>
--- js/main.js
+++ js/main.js
@@ -20,23 +20,32 @@ $(function () {
 	});
 });
 
-var fixed_block = function (fixblock_pos, i, hgth_px, hght_vh_min, item) {
+var fixed_block = function (i, hgth_px, hght_vh_min, item, style_fixed, style_unlock) {
 	"use strict";
-	var style_fixed = { //определяем стиль блокировки элемента
-			position: "fixed",
-			top: hght_vh_min[i] + 'vh'
-		},
-		style_unlock = { //тоже самое для удаления стилей с элемента
-			position: "",
-			top: ""
-		};
+	var fixblock_pos = [];
 
 	fixblock_pos[i] = hgth_px[i] - window.innerHeight / 100 * hght_vh_min[i]; //из пиксельной высоты элемента вычитаем высоту экрана умноженную на остаток высоты блока до верха, к примеру блок высотой 90% высоты экрана, от него остается 10%. как раз они и вычитаются. забыл уже для чего, по другому не работает
-	if ($(window).scrollTop() >= fixblock_pos[i]) { //если пользователь прокрутил до этой позиции..
+	if (($(window).scrollTop() >= fixblock_pos[i]) && ($(item).css('position') !== 'fixed')) { //если пользователь прокрутил до этой позиции..
 		$(item).css(style_fixed);
-	} else { //если скроллбар ничего лочить не должен, то заебись..
+	}
+	if (($(window).scrollTop() < fixblock_pos[i]) && ($(item).css('position') === 'fixed')) { //если пользователь прокрутил до этой позиции..
 		$(item).css(style_unlock); //очищаем лишние стили
 	}
+};
+
+var scroller = function (i, item, hgth_px, hght_vh_min) {
+	"use strict";
+	var fixblock_pos = [],
+		top_vh = [];
+	fixblock_pos[i] = hgth_px[i] - window.innerHeight / 100 * hght_vh_min[i];
+	top_vh[i] = Math.round($(item).position().top / $(window).height() * 100);
+	if ((top_vh[i] !== hght_vh_min[i]) && ($(window).scrollTop() > 30)) {
+		$('html, body').animate({
+			scrollTop: $(item).offset().top
+		}, 1000);
+	}
+
+
 };
 
 $(function () {
@@ -47,20 +56,43 @@ $(function () {
 		top_vh = [], //положение элемента по высоте в процентах (на самом деле без, только тсс..)
 		hght_vh = [], //высота в процентах. просто числом, на которое нужно что-то умножить
 		hgth_px = [], //высота, только в пикселях
-		hght_vh_min = []; //оставшаяся от блока высота в процентах
+		hght_vh_min = [], //оставшаяся от блока высота в процентах
+		shoved = []; //
 
 	arr.forEach(function (item, i, arr) { //для всех полученных элементов единожды запускаем, которая...
 		hgth_px[i] = $(arr[i]).position().top; //берет позицию элемента  по высоте в пикселях и...
-		top_vh[i] = Math.round($(item).position().top / $(window).height() * 100); //получаем само положение элемента по высоте, но уже в vh (процентах)
-		hght_vh[i] = Math.round($(item).height() / $(window).height() * 100); //так же и с высотой элемента
+		top_vh[i] = Math.round($(item).position().top / window.innerHeight * 100); //получаем само положение элемента по высоте, но уже в vh (процентах)
+		hght_vh[i] = Math.round($(item).height() / window.innerHeight * 100); //так же и с высотой элемента
 		hght_vh_min[i] = 100 - hght_vh[i]; //из 100% высоты экрана вычитаем место, которое способен занять элемент и получаем остаток от блока. нужен для фиксирования позиции элемента
+		shoved[i] = false;
+		fixblock_pos[i] = hgth_px[i] - window.innerHeight / 100 * hght_vh_min[i];
+		var style_fixed = { //определяем стиль блокировки элемента
+				position: "fixed",
+				top: hght_vh_min[i] + 'vh'
+			},
+			style_unlock = { //тоже самое для удаления стилей с элемента
+				position: "",
+				top: ""
+			};
 
 		$(window).resize(function () { //и при изменении размера окна (ебучий ведроид)
 			hgth_px[i] = window.innerHeight / 100 * top_vh[i]; //пишем отдельную переменную под высоту элемента. пока ебался с остальным забыл зачем нужна, но без неё не работает.		в общем, не трогать
-			fixed_block(fixblock_pos, i, hgth_px, hght_vh_min, item); //вызов функции при изменении размера окна. нужно для того чтобы всё к хуям не сломалось при ресайзе
+			fixed_block(i, hgth_px, hght_vh_min, item, style_fixed, style_unlock); //вызов функции при изменении размера окна. нужно для того чтобы всё к хуям не сломалось при ресайзе
+			fixblock_pos[i] = hgth_px[i] - window.innerHeight / 100 * hght_vh_min[i];
 		});
 		$(window).scroll(function () { //при прокрутке страницы
-			fixed_block(fixblock_pos, i, hgth_px, hght_vh_min, item); //основной вызов функции, но уже при прокрутке. 
+
+
+			if (($(window).scrollTop() >= fixblock_pos[i] - mon_hght * 0.5) && (shoved[i] === false)) {
+				$(item).children().animate({
+					opacity: 1,
+					left: 0
+				}, 1000);
+				shoved[i] = true;
+			}
+
+
+			fixed_block(i, hgth_px, hght_vh_min, item, style_fixed, style_unlock); //основной вызов функции, но уже при прокрутке.
 		});
 	});
 });
