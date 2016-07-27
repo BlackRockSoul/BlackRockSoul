b1602bdbfc92597cfd9c281b911250d01a18a7d7
BlackRockSoul <sanek-ekb@mail.ru>
2016-07-27 18:12:15 +0500

    Улучшена совсестимость для экранов с небольшими размерами. Добавлены теги h1 h2 для "seo оптимизации"

--- index.php
+++ index.php
@@ -22,26 +22,26 @@
 			<div class="sub_block1 blck_set blck1" id="bl1">
 				<div class="sub_text1 sub_set">
 					<img src="/images/photo.png" alt="BlackRockSoul" class="photo_img">
-					<h1 style="display: block; font-size: 27px">BlackRockSoul</h1>
+					<h1 style="display: block">BlackRockSoul</h1>
 					<hr>
-					<h2 style="font-size: 25px; font-weight: normal">  Зовут меня Александр, мне что-то около 18 лет. В свободное время пишу различные странные вещи по типу этого сайта. Просто для души. Знание языков так себе, но с обучением проблем нет. Главное чтобы скучно не было. А вообще стараюсь что-то делать на <strong>c#</strong>, <strong>Delphi</strong>, <strong>html</strong>, <strong>php</strong>, <strong>js</strong> и других. Всего по немногу.</h2>
+					<h2>  Зовут меня Александр, мне что-то около 18 лет. В свободное время пишу различные странные вещи по типу этого сайта. Просто для души. Знание языков так себе, но с обучением проблем нет. Главное чтобы скучно не было. А вообще стараюсь что-то делать на <strong>c#</strong>, <strong>Delphi</strong>, <strong>html</strong>, <strong>php</strong>, <strong>js</strong> и других. Всего по немногу.</h2>
 					<hr>
 				</div>
 			</div>
 			<div class="sub_block1 blck_set blck2" id="bl2">
-				<div class="sub_text2_1 sub_set"><h1 style="font-size: 27px">Проекты</h1></div>
+				<div class="sub_text2_1 sub_set"><h1>Проекты</h1></div>
 				<div class="sub_text2 sub_set">
-					<div><h2 style="font-size: 25px; font-weight: normal">Content 1</h2></div>
-					<div><h2 style="font-size: 25px; font-weight: normal">Content 2</h2></div>
-					<div><h2 style="font-size: 25px; font-weight: normal">Content 3</h2></div>
+					<div><h2>Content 1</h2></div>
+					<div><h2>Content 2</h2></div>
+					<div><h2>Content 3</h2></div>
 				</div>
 			</div>
 			<div class="sub_block1 blck_set blck3" id="bl3">
 				<div class="sub_text3 sub_set">
 					<img src="/images/photo_team.png" alt="Defend" class="photo_img">
-					<h1 style="display: block; font-size: 27px">Команда</h1>
+					<h1 style="display: block">Команда</h1>
 					<hr>
-					<h2 style="font-size: 25px; font-weight: normal">  Лучший и единственный мой помощник - Никита. Почти все проекты начинаем или заканчиваем мы в итоге вместе. И даже если изначально это был сольный проект, то это меняется очень быстро. Особо много писать не буду, Вы и сами можете почитать о нем на его сайте - <a href="http://defdev.ru/" target="_blank" style="text-decoration: underline; color: white;">defdev.ru</a></h2>
+					<h2>  Лучший и единственный мой помощник - Никита. Почти все проекты начинаем или заканчиваем мы в итоге вместе. И даже если изначально это был сольный проект, то это меняется очень быстро. Особо много писать не буду, Вы и сами можете почитать о нем на его сайте - <a href="http://defdev.ru/" target="_blank" style="text-decoration: underline; color: white;">defdev.ru</a></h2>
 					<hr>
 				</div>
 			</div>
